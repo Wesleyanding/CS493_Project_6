37fc4e30efcc844747fc27205f306d1fa5ee828e
Wesleyanding <106043198+Wesleyanding@users.noreply.github.com>
2024-05-13 12:35:21 -0700

    started user models and user login, signup

--- api/users.js
+++ api/users.js
@@ -1,4 +1,5 @@
 const { Router } = require('express')
+const jwt = require('jsonwebtoken')
 
 const { Business } = require('../models/business')
 const { Photo } = require('../models/photo')
@@ -7,9 +8,65 @@ const { Review } = require('../models/review')
 const router = Router()
 
 /*
+ * Function to require authentication
+ */
+function requireAuth(req, res, next) {
+  // Get the token from the header
+  const auth_header = req.get('Authorization') || '';
+  const auth_token = auth_header.split(' ')[1];
+
+  try {
+    jwt.verify(auth_token, process.env.JWT_SECRET);
+    req.user = payload.sub;
+    next();
+  } catch (err) {
+    res.status(401).json({ error: 'Unauthorized' })
+  }
+}
+
+
+/*
+ * Function to generate Auth token
+ */
+function generateAuthToken(user) {
+  const payload = { "sub": user };
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' });
+}
+
+/*
+ * Route to create a new user.
+ */
+router.post('/users', async function (req, res, next) {
+  try {
+    const user = await User.create(req.body, UserClientFields)
+    res.status(201).json(user)
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      res.status(400).json({ error: 'Validation error' })
+    } else {
+      next(err)
+    }
+  }
+})
+
+/*
+ * Route to login a user.
+ */
+
+router.post('/users/login', async function (req, res, next) {
+  const user = await User.findOne({ where: { email: req.body.email }})
+  if (user && user.password === hash(req.body.password)) {
+    const token = generateAuthToken(user.userId)
+    res.status(200).json({ "token": token })
+  } else {
+    res.status(401).json({ error: 'Invalid login' })
+  }
+})
+
+/*
  * Route to list all of a user's businesses.
  */
-router.get('/:userId/businesses', async function (req, res) {
+router.get('/:userId/businesses', requireAuth, async function (req, res) {
   const userId = req.params.userId
   const userBusinesses = await Business.findAll({ where: { ownerId: userId }})
   res.status(200).json({
@@ -20,7 +77,7 @@ router.get('/:userId/businesses', async function (req, res) {
 /*
  * Route to list all of a user's reviews.
  */
-router.get('/:userId/reviews', async function (req, res) {
+router.get('/:userId/reviews', requireAuth, async function (req, res) {
   const userId = req.params.userId
   const userReviews = await Review.findAll({ where: { userId: userId }})
   res.status(200).json({
@@ -31,7 +88,7 @@ router.get('/:userId/reviews', async function (req, res) {
 /*
  * Route to list all of a user's photos.
  */
-router.get('/:userId/photos', async function (req, res) {
+router.get('/:userId/photos', requireAuth, async function (req, res) {
   const userId = req.params.userId
   const userPhotos = await Photo.findAll({ where: { userId: userId }})
   res.status(200).json({
---
+++ models/user.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize')
+
+const sequelize = require('../lib/sequelize')
+const { Photo } = require('./photo')
+const { Review } = require('./review')
+const { Business } = require('./business')
+
+const User = sequelize.define('user', {
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.STRING, set(value) { this.setDataValue('password', hash(value)) }, allowNull: false },
+    admin: { type: DataTypes.BOOLEAN, allowNull: false }
+})
+
+User.hasMany(Business, { foreignKey: { allowNull: false } })
+Business.belongsTo(User)
+
+User.hasMany(Photo, { foreignKey: { allowNull: false } })
+Photo.belongsTo(User)
+
+User.hasMany(Review, { foreignKey: { allowNull: false } })
+Review.belongsTo(User)
+
+exports.User = User
+
+exports.UserClientFields = [
+    'userId',
+    'name',
+    'email',
+    'password',
+    'admin'
+]
